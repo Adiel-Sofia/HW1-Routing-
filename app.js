4a9e18dafbb6724bb206e7f45734c0b28b17a908
Adiel-Sofia <adiel13150@gmail.com>
2025-04-02 07:50:11 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+//Adiel Sofia Mendelson 206352742
+//Orly Wallela 206696775
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/users');
+const userProducts = require('./routes/products');
+const port = process.env.PORT || 3000;
+
+// Middleware for JSON processing (if required)
+app.use(express.json());
+app.use(express.static('public'));
+
+// Routers
+app.use('/api/users', userRoutes); // All user routes
+app.use('/api/products', userProducts); //All products routes
+
+// Main Page
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to the Main Page</h1>');
+});
+
+// Processing route 404
+app.use((req, res) => {
+  res.status(404).send('<h1>404 Not Found</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
